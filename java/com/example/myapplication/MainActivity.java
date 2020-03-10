1da50525a64d0ef8edf404b9c36b115b9f0a56a3
Ilya Zaberezhnyy <izaber@list.ru>
2020-03-10 21:53:37 +0600

    something

--- java/com/example/myapplication/MainActivity.java
+++ java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Mother", "FUCKER");
+        Log.e("Hello world!", "there is some text");
     }
 }
